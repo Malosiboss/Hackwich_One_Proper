7eed104b8aefa0cde73087184ee82e647e1c3c7c
NoahNua <NoahNua@hawaii.edu>
2021-01-13 20:20:30 -1000

    Initial Commit

---
+++ Hackwich_OneTests/Hackwich_OneTests.swift
@@ -0,0 +1,33 @@
+//
+//  Hackwich_OneTests.swift
+//  Hackwich_OneTests
+//
+//  Created by Noah Nua on 1/13/21.
+//
+
+import XCTest
+@testable import Hackwich_One
+
+class Hackwich_OneTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
